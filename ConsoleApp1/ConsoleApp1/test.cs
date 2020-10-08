0053906278bd0488d05be1ff56b41dffca03b799
ShahrulX <shahrul@sophicautomation.com>
2020-10-08 12:48:54 +0800

    push

---
+++ ConsoleApp1/ConsoleApp1/test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class test
+    {
+        public void a()
+        {
+            var c = 1;
+        }
+    }
+}
