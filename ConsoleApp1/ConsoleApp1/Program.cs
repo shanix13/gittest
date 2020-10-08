62b7bb185718aba7ef2093a676cf5498e411c202
ShahrulX <shahrul@sophicautomation.com>
2020-10-08 12:58:00 +0800

    program change

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,8 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World2!");
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World12!");
         }
     }
 }
